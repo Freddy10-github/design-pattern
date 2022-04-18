f5bb46111370f5973b3816003c08b9c10ba6bd1e
Freddylo <freddy_luo@moneynet.tw>
2022-04-18 23:01:11 +0800

    feature: interface

---
+++ src/ts/any-unknow-as/index.ts
@@ -0,0 +1,54 @@
+
+
+
+
+// let x: string; 
+
+
+// const y = [1, 2];
+
+// y.push('hello');
+
+// let x:any = 10;
+
+// let x1: number = x;
+// let x2: string = x;
+// let x3: string[] = x;
+// let x4: [string, boolean] = x;
+// let x5: null = x;
+// let x6: undefined = x;
+// let x7: any = x;
+
+// x7 = x1;
+// x7 = x2;
+// x7 = x3;
+// x7 = x4;
+// x7 = x5;
+// x7 = x6;
+
+
+const ifHasProduct = true;
+
+
+function getProduct () {
+  let productName: unknown;
+
+  productName  = ifHasProduct ? 'Apple' : null;
+
+  return productName;
+}
+
+const product = getProduct();
+
+if(typeof product == 'string'){
+  product.split('')
+}
+
+
+
+// const fetchData = async () => {
+//   const resp = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//   const data = resp.json() as unknown as {userId: number, id: number, title: string, completed: boolean}
+// }
+4
+// export {};
---
+++ src/ts/type-interface/type.ts
@@ -0,0 +1,18 @@
+type customerInfo = {name: string, id: number, get: () => string};
+
+let A: customerInfo;
+
+
+
+A = {
+  name: 'Hans'
+}
+
+
+
+type pets =  'dog' | 'cat' | 'fish'
+
+let myPet:pets = 'dog'
+
+
+export {};
